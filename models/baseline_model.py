c2ba4169bd17551f0e605bbc78747bba1b991a7e
Zen <zhengxu5511@gmail.com>
2018-01-19 13:38:37 +0000

    layer test

--- models/baseline_model.py
+++ models/baseline_model.py
@@ -22,18 +22,18 @@ def create_network(img_prep, img_aug, learning_rate):
                          data_preprocessing=img_prep,
                          data_augmentation=img_aug)
     # First convolution layer. 32 filters of size 5. Activation function ReLU. 64x64x3 -> 64x64x32
-    network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
-    network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
-    network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
+    network = conv_2d(network, 64, 3, activation='relu', padding='SAME')
+    # network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
+    # network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
     network = max_pool_2d(network, 2)
     # Second convolution layer. 32 filters of size 5. Activation function ReLU. 32x32x32 -> 32x32x32
     network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
-    network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
-    network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
+    network = max_pool_2d(network, 2)
+    # network = conv_2d(network, 32, 3, activation='relu', padding='SAME')
+    network = conv_2d(network, 16, 3, activation='relu', padding='SAME')
+    network = max_pool_2d(network, 2)
     # First fully connected layer. 32x32x32 -> 1x32768 -> 1x1024. ReLU activation.
     network = fully_connected(network, 1024, activation='relu')
-    # Third batch normalization layer
-    network = batch_normalization(network, stddev=0.002, trainable=True, restore=True, reuse=False)
     # Dropout layer for the first fully connected layer.
     network = dropout(network, 0.5)
     # Second fully connected layer. 1x1024 -> 1x200. Maps to class labels. Softmax activation to get probabilities.
