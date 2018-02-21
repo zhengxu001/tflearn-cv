432a4ad0fe2473862f290e773bc6037188f19305
Zen <zhengxu5511@gmail.com>
2018-02-21 20:34:11 +0000

    add print

--- train.py
+++ train.py
@@ -102,7 +102,8 @@ def main(data_dir, hdf5, name):
                         )
     model2 = tflearn.DNN(network, tensorboard_verbose=0, tensorboard_dir='tensorboard', best_checkpoint_path=checkpoint_path)
     model2.load(name)
-    model.evaluate(X_test, Y_test, batch_size=batch_size)
+    a = model.evaluate(X_test, Y_test, batch_size=batch_size)
+    p a
 
 if __name__ == '__main__':
     # Parse arguments and create output directories.
