5258b397c37d038c8d051fd949d0b5d6a054ce00
Zen <zhengxu5511@gmail.com>
2018-02-22 01:37:30 +0000

    update

--- batch_train.py
+++ batch_train.py
@@ -4,8 +4,8 @@ augmentations = ["FLIP"]
 # epochs = [20, 40, 60]
 epochs = [50]
 # models = ["alex", "vgg", "res", "alch"]
-models = ["alex", "vgg", "res"]
-
+# models = ["alex", "vgg", "res"]
+models = ["vgg", "res"]
 for aug in augmentations:
 	for epoch in epochs:
 		for model in models:
