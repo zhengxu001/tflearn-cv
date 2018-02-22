30934c2980e8f6817aa653ec555c2342beb9edad
Zen <zhengxu5511@gmail.com>
2018-02-22 17:47:15 +0000

    test vgg

--- batch_train.py
+++ batch_train.py
@@ -1,12 +1,8 @@
 import train
-# augmentations = ["NA", "FLIP"]
-augmentations = ["FLIP"]
-# epochs = [20, 40, 60]
-# epochs = [50]
-epochs = [10]
-# models = ["alex", "vgg", "res", "alch"]
-models = ["alex", "vgg", "res"]
-# models = ["baseline"]
+augmentations = ["NA", "FLIP"]
+epochs = [2]
+# models = ["alex", "vgg", "res"]
+models = ["vgg", "res"]
 for aug in augmentations:
 	for epoch in epochs:
 		for model in models:
