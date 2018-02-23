c6a7757b9272823069e3dd04978b3f5159abc382
Zen <zhengxu5511@gmail.com>
2018-02-23 22:50:27 +0000

    train alchNet

--- batch_train.py
+++ batch_train.py
@@ -1,7 +1,7 @@
 import train
 # augmentations = ["NA", "FLIP"]
-augmentations = ["FLIP"]
-epochs = [65]
+# augmentations = ["FLIP"]
+# epochs = [65]
 # models = ["alex", "vgg", "res"]
 # models = ["vgg", "res"]
 # models = ["res"]
@@ -10,8 +10,11 @@ epochs = [65]
 # 		for model in models:
 # 			train.main(model+"-"+aug+"-"+str(epoch), epoch, aug, model)
 
-self_models = ["alch11_without_dropout", "alch11", "alch19"]
-for aug in augmentations:
-	for epoch in epochs:
-		for model in self_models:
-			train.main(model+"-"+aug+"-"+str(epoch), epoch, aug, model)
+# self_models = ["alch11_without_dropout", "alch11", "alch19"]
+# for aug in augmentations:
+# 	for epoch in epochs:
+# 		for model in self_models:
+# 			train.main(model+"-"+aug+"-"+str(epoch), epoch, aug, model)
+
+train.main("alch11-FLIP-65", 65, "FLIP", "alch11")
+train.main("alch19-FLIP-72", 72, "FLIP", "alch19")
