1060cfd4a6af909c1c7be494b83706e9d2097c2d
Zen <zhengxu5511@gmail.com>
2018-02-25 11:07:46 +0000

    bug fix

--- models/alchNet19.py
+++ models/alchNet19.py
@@ -22,7 +22,7 @@
 #     return network
 
 from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d, avg_pool_2d
+from tflearn.layers.conv import conv_2d, max_pool_2d, avg_pool_2d, global_avg_pool
 from tflearn.layers.estimator import regression
 from tflearn.layers.normalization import batch_normalization
 from tflearn.activations import relu
@@ -56,13 +56,10 @@ def alchNet19(img_prep, img_aug, learning_rate):
     network = resLayer(network, 256)
     network = resLayer(network, 512, stride = 2)
     network = resLayer(network, 512)
-
+    network = global_avg_pool(network)
     network = fully_connected(network, 1024, activation='relu')
     network = batch_normalization(network, stddev=0.002, trainable=True, restore=True, reuse=False)
     network = dropout(network, 0.5)
-    # TODO: 
-    # should try this out
-    network = global_avg_pool(network)
     network = fully_connected(network, 200, activation='softmax')
     network = regression(network, optimizer='momentum',
                          loss='categorical_crossentropy',
